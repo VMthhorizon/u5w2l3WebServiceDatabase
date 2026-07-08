23d88c767fe313432dcfe08297876f12be365215
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-07-08 15:03:53 +0200

    Initial commit

---
+++ src/main/java/vincenzo/u5w2l3WebServiceDatabase/U5w2l3WebServiceDatabaseApplication.java
@@ -0,0 +1,13 @@
+package vincenzo.u5w2l3WebServiceDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w2l3WebServiceDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w2l3WebServiceDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vincenzo/u5w2l3WebServiceDatabase/U5w2l3WebServiceDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzo.u5w2l3WebServiceDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w2l3WebServiceDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
